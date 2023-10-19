3c157b28a83535b827d5fd8a8d03cc0ed0447989
Hai Nam Nguyen <49573677+nammmx@users.noreply.github.com>
2023-10-19 11:00:08 +0700

    Update functions_and_variables.py

--- functions_and_variables.py
+++ functions_and_variables.py
@@ -33,6 +33,7 @@ custom_css = {
 }
 #################################################################################################### Functions
 def create_sidebar():
+    st.sidebar.write("")
     st.sidebar.subheader("Quick Queries")
     st.sidebar.write("")
     st.sidebar.write("Data Query (DQL)")
