d8f692e48fa6eba50d2bb98e6469f3be8f40151c
Hai Nam Nguyen <49573677+nammmx@users.noreply.github.com>
2023-11-11 12:34:37 +0100

    Update 2_Admin.py

--- pages/2_Admin.py
+++ pages/2_Admin.py
@@ -132,7 +132,7 @@ if check_password():
     
         df = pd.DataFrame(list(zip(options_id, options_date, options_sql, options_db, options_question)),
                 columns =['ID', 'Date', 'SQL Topics', 'DB Topics', 'Question'])
-        df = df.sort_values(["Date","ID"], ascending=[False,False])
+        df = df.sort_values(["Date","ID"], ascending=[False,True])
     except Exception as error:
         st.write(error)
     
